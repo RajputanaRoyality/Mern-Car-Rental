9184d994420ef2cf0b81d16b2ac89d8fedbffbbf
RajputanaRoyality <vaibhavrksingh6207@gmail.com>
2025-06-30 12:24:48 +0530

    first commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import Hero from '../components/Hero'
-import CarCard from '../components/carCard'
-import { dummyCarData } from '../assets/assets'
 import FeaturedDestination from '../components/FeaturedSection.jsx'
 import Banner from '../components/Banner.jsx'
 import Testimonial from '../components/Testinomials.jsx'
